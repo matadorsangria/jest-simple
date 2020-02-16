7b7cde1cfdff1d27dfb24032952361dbfbe3e57d
江原 圭 <matador.sangria@gmail.com>
2020-02-16 22:25:17 +0900

    Add: createAbValue

---
+++ tests/createAbValue.test.ts
@@ -0,0 +1,132 @@
+import createAbValue from "../createAbValue";
+
+describe('createAbValue', (): void => {
+  // 異常系
+  test('会員番号が空文字', (): void => {
+    expect(() => {
+      createAbValue({
+        id: ''
+      })
+    }).toThrow(Error);
+  });
+
+  test('桁目が7', (): void => {
+    expect(() => {
+      createAbValue({
+        id: '1234567',
+        digit: 7
+      })
+    }).toThrow(RangeError);
+  });
+
+  test('桁範囲が0', (): void => {
+    expect(() => {
+      createAbValue({
+        id: '1234567',
+        range: 0
+      })
+    }).toThrow(RangeError);
+  });
+
+  test('桁範囲が3', (): void => {
+    expect(() => {
+      createAbValue({
+        id: '1234567',
+        range: 3
+      })
+    }).toThrow(RangeError);
+  });
+
+  test('テストパターンが1', (): void => {
+    expect(() => {
+      createAbValue({
+        id: '1234567',
+        pattern: 1
+      })
+    }).toThrow(RangeError);
+  });
+
+  test('テストパターンが11', (): void => {
+    expect(() => {
+      createAbValue({
+        id: '1234567',
+        pattern: 11
+      })
+    }).toThrow(RangeError);
+  });
+
+  test('桁範囲が会員番号を超える', (): void => {
+    expect(() => {
+      createAbValue({
+        id: '1234567',
+        range: 20,
+      })
+    }).toThrow(RangeError);
+  });
+
+  test('会員番号があ', (): void => {
+    expect(() => {
+      createAbValue({
+        id: 'あ'
+      })
+    }).toThrow(TypeError);
+  });
+
+
+  // 正常系
+  test('会員番号が1、下1桁目、ABテスト', (): void => {
+    const result: string = createAbValue({
+      id: '1'
+    });
+    expect(result).toEqual('B');
+  });
+
+  test('会員番号が1234567899、10桁、ABCDEFGHIJテスト', (): void => {
+    const result: string = createAbValue({
+      id: '1234567899',
+      pattern: 10
+    });
+    expect(result).toEqual('J');
+  });
+
+  test('会員番号が123456、下2〜3桁目、ABCテスト', (): void => {
+    const result: string = createAbValue({
+      id: '123456',
+      digit: 2,
+      range: 2,
+      pattern: 3
+    });
+    expect(result).toEqual('A');
+  });
+
+  test('会員番号が123466、下2〜3桁目、ABCテスト', (): void => {
+    const result: string = createAbValue({
+      id: '123466',
+      digit: 2,
+      range: 2,
+      pattern: 3
+    });
+    expect(result).toEqual('B');
+  });
+
+  test('会員番号が123476、下2〜3桁目、ABCテスト', (): void => {
+    const result: string = createAbValue({
+      id: '123476',
+      digit: 2,
+      range: 2,
+      pattern: 3
+    });
+    expect(result).toEqual('C');
+  });
+
+  test('会員番号が123996、下2〜3桁目（ABテスト非対象パターン）', (): void => {
+    const result: string = createAbValue({
+      id: '123996',
+      digit: 2,
+      range: 2,
+      pattern: 3
+    });
+    expect(result).toEqual('');
+  });
+
+});
